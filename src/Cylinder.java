7fab85eb6c0e13d48e75540bc266782035573989
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-04-09 11:21:43 +0700

    Method

---
+++ src/Cylinder.java
@@ -0,0 +1,17 @@
+public class Cylinder {
+    public static double getVolume(int radius, int height){
+        double baseArea = getBaseArea(radius);
+        double perimeter = getPerimeter(radius);
+        double volume = perimeter * height + 2 * baseArea;
+        return volume;
+    }
+
+    private static double getPerimeter(int radius) {
+        return 2 * Math.PI  * radius;
+    }
+
+    private static double getBaseArea(int radius) {
+        double baseArea = Math.PI * radius * radius;
+        return baseArea;
+    }
+}
